3c53d8c73988bd8be468f1506891712331343c5c
Cong <congusbongus@gmail.com>
2024-04-24 22:07:43 +1000

    Fix palette

--- convert.py
+++ convert.py
@@ -1,6 +1,8 @@
 from PIL import Image
 import numpy as np
 im = Image.open("image.png")
+# Quantize to 16 colors
+im = im.convert('P', palette=Image.ADAPTIVE, colors=16)
 indexed = np.array(im)
 indices = [str(num) for row in indexed for num in row]
 # Use RLE so the image hopefully doesn't exceed code size limits
@@ -16,4 +18,4 @@ print(f"img_values = {{{','.join(values)}}}")
 print(f"img_runs = {{{','.join(str(run) for run in runs)}}}")
 palette = im.getpalette()
 print(f"pal = {{{','.join(str(p) for p in palette[:16*3])}}}")
-print(f"-- pal = {''.join(hex(p)[2:] for p in palette[:16*3])}")
+print(f"-- pal = {''.join(f'{p:02x}' for p in palette[:16*3])}")
